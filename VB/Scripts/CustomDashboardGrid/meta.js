d13754e671b6fbe17ac898288fe4734c76b6bd16
DevExpressExampleBot <fakeEmail>
2019-05-30 18:08:11 +0300

    Source auto update [skip ci]

---
+++ VB/Scripts/CustomDashboardGrid/meta.js
@@ -0,0 +1,27 @@
+﻿var customItemExtensionMeta = {
+    bindings: [
+        {
+            propertyName: 'dimensionsBinding',
+            dataItemType: 'Dimension',
+            array: true,
+            displayName: 'Dimensions',
+            placeholder: 'Add Dimension',
+            configurePlaceholder: 'Configure Dimension',
+            enableInteractivity: true
+        },
+        {
+            propertyName: 'measuresBinding',
+            dataItemType: 'Measure',
+            array: true,
+            displayName: 'Measures',
+            placeholder: 'Add Measure',
+            configurePlaceholder: 'Configure Measure'
+        }
+    ],
+    interactivity: {
+        filter: true,
+    },
+    groupName: 'common',
+    title: 'SourceRowGrid',
+    index: 20
+};
