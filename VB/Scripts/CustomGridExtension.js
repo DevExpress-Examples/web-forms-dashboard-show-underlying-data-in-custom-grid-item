b0f7291f536e455fb238edfe5285f6eab805b7dd
Sergey Kanaev <sergey.kanayev@devexpress.com>
2022-08-05 11:20:00 +0400

    Update custom item to ES6 syntax

--- VB/Scripts/CustomGridExtension.js
+++ VB/Scripts/CustomGridExtension.js
@@ -1,4 +1,4 @@
-var MyDashboardGrid = (function () {
+window.MyDashboardGrid = (function () {
 	var Dashboard = DevExpress.Dashboard;
 	var dxDataGrid = DevExpress.ui.dxDataGrid;
 
@@ -32,63 +32,65 @@ var MyDashboardGrid = (function () {
 	};
 	
 	// Specifies an item viewer behavior.
-	var Viewer = function(dashboardControl, model, container, options) {
-		Dashboard.CustomItemViewer.call(this, model, container, options);
-		this.viewerApiExtension = dashboardControl.findExtension('viewer-api');
-	}
-	Viewer.prototype = Object.create(Dashboard.CustomItemViewer.prototype);
-	Viewer.prototype.constructor = Viewer;
-	Viewer.prototype.renderContent = function ($element, changeExisting) {
-		//Dimensions and Measures
-		var requestedDataMembers = [];
-		var bindings = this.getBindingValue('dimensionsBinding').concat(this.getBindingValue('measuresBinding'));
-		bindings.forEach(function (binding) {
-			requestedDataMembers.push(binding.displayName());
-		});
-			
-		//Create a div with a grid
-		var div = document.createElement('div');
-		var dataGrid = new dxDataGrid(div, { height: '100%' });
-			
-		//Add a div with a grid to a container
-		var container = $element.jquery ? $element[0] : $element;
-		while(container.firstChild)
-			container.removeChild(container.firstChild);
-		container.appendChild(div);
-			
-		//Prepare parameters
-		var requestParameters = {
-			DataMembers: requestedDataMembers,
-			AxisPoints: []
-		};
-			
-		//Request underlying data and fill a data source
-		this.viewerApiExtension && this.viewerApiExtension.requestUnderlyingData(this.getName(), requestParameters, function (data) {
-			var underlyingData = [];
-			var dataMembers = data.getDataMembers();
-			for (var i = 0; i < data.getRowCount() ; i++) {
-				var dataTableRow = {};
-				dataMembers.forEach(function(dataMember) {
-					dataTableRow[dataMember] = data.getRowValue(i, dataMember);
-				});
-					
-				underlyingData.push(dataTableRow);
-			}
-			//Set a datasource to a grid
-			dataGrid.option('dataSource', underlyingData);
-		});
-	};
-	
-	// Creates function that implements a custom item extension.	
-	function MyDashboardGrid(dashboardControl) {
-		Dashboard.ResourceManager.registerIcon(svgIcon);
-		
-		this.name = 'MyDashboardGrid';
-		this.metaData = customItemExtensionMeta;
-		this.createViewerItem = function (model, $element, content) {
-			return new Viewer(dashboardControl, model, $element, content);
+	class Viewer extends Dashboard.CustomItemViewer {
+		constructor(dashboardControl, model, container, options) {
+			super(model, container, options);
+			this.viewerApiExtension = dashboardControl.findExtension('viewer-api');
 		}
-	};
+		renderContent($element, changeExisting) {
+			//Dimensions and Measures
+			var requestedDataMembers = [];
+			var bindings = this.getBindingValue('dimensionsBinding').concat(this.getBindingValue('measuresBinding'));
+			bindings.forEach(function (binding) {
+				requestedDataMembers.push(binding.displayName());
+			});
+				
+			//Create a div with a grid
+			var div = document.createElement('div');
+			var dataGrid = new dxDataGrid(div, { height: '100%' });
+				
+			//Add a div with a grid to a container
+			var container = $element.jquery ? $element[0] : $element;
+			while(container.firstChild)
+				container.removeChild(container.firstChild);
+			container.appendChild(div);
+				
+			//Prepare parameters
+			var requestParameters = {
+				DataMembers: requestedDataMembers,
+				AxisPoints: []
+			};
+				
+			//Request underlying data and fill a data source
+			this.viewerApiExtension && this.viewerApiExtension.requestUnderlyingData(this.getName(), requestParameters, function (data) {
+				var underlyingData = [];
+				var dataMembers = data.getDataMembers();
+				for (var i = 0; i < data.getRowCount() ; i++) {
+					var dataTableRow = {};
+					dataMembers.forEach(function(dataMember) {
+						dataTableRow[dataMember] = data.getRowValue(i, dataMember);
+					});
+						
+					underlyingData.push(dataTableRow);
+				}
+				//Set a datasource to a grid
+				dataGrid.option('dataSource', underlyingData);
+			});
+		}
+	}
+	// Creates function that implements a custom item extension.	
+	class MyDashboardGrid {
+		constructor(dashboardControl) {
+			Dashboard.ResourceManager.registerIcon(svgIcon);
+			
+			this.name = 'MyDashboardGrid';
+			this.metaData = customItemExtensionMeta;
+			this.dashboardControl = dashboardControl;
+		}
+		createViewerItem = (model, $element, content) => {
+			return new Viewer(this.dashboardControl, model, $element, content);
+		}
+	}
 	
 	return MyDashboardGrid;
 })();
