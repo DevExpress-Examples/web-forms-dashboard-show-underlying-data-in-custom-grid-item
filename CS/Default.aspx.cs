9c5a63580ab9e98ff500e6db22dda7e2dd4b88fe
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-02 17:54:41 +0400

    Change example status to out of maintenance

--- CS/Default.aspx.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace WebApplication18 {
-    public partial class Default : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
-
-        }
-    }
-}
--- VB/Global.asax
+++
@@ -1,1 +0,0 @@
-<%@ Application Codebehind="Global.asax.vb" Inherits="WebApplication18.Global" Language="VB" %>
